397e4779e9995e3737d7b94d50bb792731fa117e
Patrick Segura <psegur@Patricks-MacBook-Pro-2.local>
2017-01-21 19:37:19 -0600

    Added Demo video, and removed an unused route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = (app) => {
 
-    app.get('/survey', (req, res) => {
-      res.sendFile(path.join(__dirname, "/../public/survey.html"));
-    });
+    // app.get('/survey', (req, res) => {
+    //   res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    // });
     app.get('/results', (req, res) => {
       res.sendFile(path.join(__dirname, "/../public/results.html"));
     });
